44ff1a8eeb601823ce71af77c79023b0976dee06
michaeliyaomolere <mikoa04@gmail.com>
2025-03-01 13:08:41 -0500

    Fix build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config optioouns here */
+  output: "export",
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
 import GroceryList from "@/components/GroceryList";
-import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  const user = await currentUser();
-
   return (
     <main className="min-h-screen bg-gray-50 py-8">
       <div className="max-w-4xl mx-auto px-4">
-        {user && (
+        {/* {user && (
           <h1 className="text-2xl font-bold text-center mb-2 text-gray-800">
             Hello {user?.fullName}
           </h1>
-        )}
+        )} */}
         <h1 className="text-3xl font-bold text-center mb-2 text-gray-800">
-          {user?.firstName} Family HouseHold List
+          Iyaomolere Family HouseHold List
         </h1>
         <div className="text-center text-gray-800">
           Please insert items needed for the household
