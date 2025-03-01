09391af3c421d30469857fac3cc2dc18523b80c3
michaeliyaomolere <mikoa04@gmail.com>
2025-03-01 11:48:59 -0500

    Implement Clerk for user signin and signup

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <SignIn
+        appearance={{
+          elements: {
+            formButtonPrimary:
+              "bg-slate-500 hover:bg-slate-400 text-sm normal-case",
+            footerActionLink: "text-slate-500 hover:text-slate-400",
+          },
+        }}
+        afterSignInUrl="/"
+        signUpUrl="/sign-up"
+      />
+    </div>
+  );
+}
