494a86ad953097dea52a251ecfb5fcc4d73e5755
michaeliyaomolere <mikoa04@gmail.com>
2025-03-01 23:01:54 -0500

    Implement the sharable link

--- src/components/UrlDisplay.tsx
+++ src/components/UrlDisplay.tsx
@@ -1,16 +1,11 @@
-"use client";
-
-import { usePathname } from "next/navigation";
 interface ListProps {
   userId: string;
 }
 export default function UrlDisplay({ userId }: ListProps) {
-  const pathname = usePathname();
-
+  const webUrl = process.env.APP_LIST_URL;
   return (
     <div className="text-center text-sm text-gray-500 mt-2 mb-4">
-      Share this list using: {window.location.origin}
-      {pathname}
+      Share this list using: {webUrl}
       {userId}
     </div>
   );
